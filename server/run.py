21406e5f4b9b9227c31b1422818cecdbb87ef0db
Nissan <mdnissanali130@gmail.com>
2025-09-15 03:43:39 +0600

    back again to development

--- server/run.py
+++ server/run.py
@@ -1,8 +1,7 @@
 from app import create_app
+from app.config import get_config
 
-# Create Flask app instance using factory
-app = create_app()
+app = create_app(get_config())
 
-if __name__ == '__main__':
-    # Run Flask server
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
