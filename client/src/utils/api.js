28b0b228a343cd1b47650971daca8ddfc612a79a
Nissan <mdnissanali130@gmail.com>
2025-09-15 06:29:20 +0600

    server redeploy

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -6,4 +6,4 @@ const isDevelopment = window.location.hostname === "localhost";
 // Set API base URL based on environment
 export const API_BASE_URL = isDevelopment
   ? "http://127.0.0.1:5000" // Local Flask server
-  : "https://exam-app-api-f5ge.onrender.com"; // Render deployment
+  : "https://exam-app-server.onrender.com"; // Render deployment
